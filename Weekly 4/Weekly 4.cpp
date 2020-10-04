ecd282f283f575e36e4baf8e2a76e95a1d3db083
Kigaroth <hp_20@hotmail.com>
2020-10-04 15:31:34 +0200

    Dett er en ny test

--- Weekly 4/Weekly 4.cpp
+++ Weekly 4/Weekly 4.cpp
@@ -1,20 +1,38 @@
-// Weekly 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Weekly 4 - Task 1.cpp //
 
 #include <iostream>
+#include <string>
+#include <locale>
+
+
+std::string sentence;
+
+
+void input_sentence() {
+
+    std::cout << "Please type a sentence: ";
+    std::getline(std::cin, sentence);
+    std::cout << std::endl;
+
+}
+
+void capitalize_sentence() {
+
+    std::cout << "\n The all capitalized version of ''" << sentence << "'' is:\n ";
+    
+    for (int i = 0; i < sentence.length(); i++) {
+        putchar(toupper(sentence[i]));
+    }
+    std::cout << std::endl;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    input_sentence();
+    capitalize_sentence();
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
