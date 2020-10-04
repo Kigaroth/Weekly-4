7f16f76dd5fab9b28ca672666c30b33327f4ce3d
Kigaroth <hp_20@hotmail.com>
2020-10-04 16:27:17 +0200

    Test

--- Weekly 4/Weekly 4.cpp
+++ Weekly 4/Weekly 4.cpp
@@ -1,12 +1,32 @@
-// Weekly 4 - Task 1.cpp //
+// Weekly 4.cpp //
 
 #include <iostream>
 #include <string>
-#include <locale>
+#include <vector>
+#include <conio.h>
 
 
+
+// Task 1
 std::string sentence;
 
+// Task 2
+
+const int size = 10;
+char player = 64;
+int array[size]{};
+std::vector <char> board(100, ' ');
+
+// Task 3
+
+
+
+struct phonebook {
+    std::string name;
+    int phone_number;
+};
+
+// Task 1 - Functions
 
 void input_sentence() {
 
@@ -26,13 +46,40 @@ void capitalize_sentence() {
     std::cout << std::endl;
 }
 
+//Task 2 - Functions
 
+void display_board() {
+    for (int i = 0; i < board.size(); i++) {
+        if (i % size == 0) {
+            std::cout << "\n-----------------------------------------\n| ";
+        }
+        std::cout << board[i] << " | ";
+    }
+    std::cout << "\n-----------------------------------------";
+    std::cout << std::endl;
+}
+
+void player_input() {
+
+
+}
+
+//Task 3 - Functions
+
+phonebook get_phonebook_data[10] {
+
+    phonebook 
+
+}
+
+void print_phonebook( phonebook) {
+
+}
 
 int main()
 {
 
-    input_sentence();
-    capitalize_sentence();
+    
 
 }
 
